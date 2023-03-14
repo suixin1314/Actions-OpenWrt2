39a8e72a222fa81a3a74eac5e1ff09c1e1fc04ac
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2023-03-14 18:57:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+git clone https://github.com/Zxilly/UA2F package/UA2F
+git clone https://github.com/kenzok8/openwrt-packages.git 
+git clone https://github.com/kenzok8/small.git
