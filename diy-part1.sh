903fddd4f0accdc750095bda4fe888f31b3bbddc
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2023-03-20 18:25:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git zxilly https://github.com/Zxilly/UA2F' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i 'src-git small https://github.com/kenzok8/small' feeds.conf.default
